8fd6737dda1206b1e1fedf4824497b3c448cf224
Charles Howard <cdhoward@add123.com>
2013-10-01 17:28:20 -0400

    started on new app structure

--- require_config.js
+++ require_config.js
@@ -10,8 +10,11 @@
 		//never includes a ".js" extension since
 		//the paths config could be for a directory.
 		paths: {
-			jquery: '/lib/jquery/jquery-2.0.0',
-			jqueryui: "jqueryui/jquery-ui"
+			jquery: 'jquery/jquery-2.0.0',
+			jqueryui: "jqueryui/jquery-ui",
+			doTCompiler:  'doT',
+			text: 'text',
+			doT: 'requirejs-doT'
 		},
 		shim: {
 			jqueryui: {
@@ -21,6 +24,22 @@
 		},
 		callback: function () {
 			console.log('loaded requirejs config');
+		},
+		doT: {
+			ext: '.dot.html', // extension of the templates, defaults to .dot
+			templateSettings: {
+				evaluate: /\{\{([\s\S]+?)\}\}/g,
+				interpolate: /\{\{=([\s\S]+?)\}\}/g,
+				encode: /\{\{!([\s\S]+?)\}\}/g,
+				use: /\{\{#([\s\S]+?)\}\}/g,
+				define: /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
+				conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
+				iterate: /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
+				varname: 'data',
+				strip: true,
+				append: true,
+				selfcontained: false
+			}
 		}
 	});
 }());
